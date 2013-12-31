6ed51abbdcf7da11301bdfd9c8249c5bc96ebb10
jh222jg <johan.holmberg123@hotmail.com>
2013-12-31 17:04:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ require.config({
         jquery: "src/lib/jquery/jquery",
         underscore: "src/lib/underscore-amd/underscore",
         backbone: "src/lib/backbone-amd/backbone",
-        localstorage: "src/lib/backbone.localstorage/backbone.localstorage",
+        localstorage: "src/lib/backbone.localstorage/backbone.localstorage.min",
 
         router: "src/router",
 
